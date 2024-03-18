27dabeea8120e679d33ffbd8d9dccbd2ed33b586
Mis-nomer <hoangchilam@pm.me>
2024-03-18 14:31:19 +0700

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['standard-with-typescript', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/consistent-type-assertions': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      extends: 'love',
+    },
+  ],
+};
